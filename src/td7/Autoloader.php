bdf44cd62970987aabfd8a58b5d7f653e09981fe
NaelGuenfoudi <guenfmen@gmail.com>
2022-09-28 17:36:20 +0200

    ajout des methodes de l'autoloader

--- src/td7/Autoloader.php
+++ src/td7/Autoloader.php
@@ -1,7 +1,22 @@
 <?php
 
 class Autoloader {
+   private string $prefixe;
+    private string $chemin;
 
-    public function __construct(string $prefixe)
+    public function __construct(string $prefixeP,string $cheminP){
+        $this->prefixe=$prefixeP;
+        $this->chemin=$cheminP;
+    }
+    public function loadClass(string $nameclass)
+    {
+        $path_to_file = $this->chemin . $nameclass . 'php';
+        if (file_exists($path_to_file)) {
+            require_once $path_to_file;
+        }
+    }
+    public function register(){
+        spl_autoload_register([__CLASS__,'autoload']);
+    }
 
 }
